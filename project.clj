f13f1d18bda9e95652bc01b286c9736c07f272aa
Rosnec <rosnec.dev@gmail.com>
2014-05-07 23:09:48 -0400

    adding jar names to project.clj

--- project.clj
+++ project.clj
@@ -6,4 +6,6 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [seesaw "1.4.4"]]
   :java-source-paths ["src/util/java"]
+  :jar-name "gpckwlf.jar"
+  :uberjar-name "gpckwlf-standalone.jar"
   :main gpckwlf.core)
